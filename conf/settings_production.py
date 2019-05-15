571ce3684b6930a35cb9c75cc1d394dd453d07f0
sundayle <shaopeng1995@gmail.com>
2019-05-15 14:28:19 +0800

    bk

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -11,7 +11,7 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': 'bkclass_dev',                            # 数据库名 (默认与APP_ID相同)
+        'NAME': 'bkclass_prod',                            # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
         'PASSWORD': 'Uqv.83WuNm',                        # 你的数据库password
         'HOST': '10.0.1.102',                   		   # 数据库HOST
