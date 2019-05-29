a52bbe7dcc346400bbb5a541b2702c705fb0309c
sundayle <shaopeng1995@gmail.com>
2019-05-29 16:05:32 +0800

    dete

--- get_capacity/api_urls.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.conf.urls import patterns
-
-# 提供磁盘容量历史数据查询API
-urlpatterns = patterns('get_capacity.api_views',
-    (r'^history_data/$', 'get_history_data'),
-)
--- iwork/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.conf.urls import patterns
-
-urlpatterns = patterns('iwork.views',
-    (r'^$', 'home'),
-    (r'^save_record/$', 'save_record'),
-    (r'^records/$', 'records'),
-)
